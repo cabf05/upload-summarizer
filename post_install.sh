354d37b9a68e92827f4c9fafa225b28231f62ee9
cabf05 <cabf05@gmail.com>
2025-02-02 18:54:40 -0500

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
+#!#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y tesseract-ocr tesseract-ocr-por
 python -c "import nltk; nltk.download('stopwords'); nltk.download('punkt'); nltk.download('wordnet'); nltk.download('omw-1.4')"
